86578a06a1e6aa3f13d13c4971f9f8465974d957
0marA <theengineermachine@gmail.com>
2023-05-01 19:34:37 -0500

    Home page

--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -2,37 +2,35 @@ import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate, useParams } from "react-router-dom";
 import { auth, logInWithEmailAndPassword, createNewGame } from "../firebase";
-import { Button } from "react-bootstrap"
+import { Button } from "react-bootstrap";
+import "../styles/index.css";
 
 const Create = () => {
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
 
-    // jacob i have to commit rn
-    // so just seee the gc
+  const submitHandler = (e) => {
+    e.preventDefault();
 
-    const [name, setName] = useState("");
-    const navigate = useNavigate();
+    let newId;
+    createNewGame(name).then((id) => {
+      newId = id;
+    });
+    new navigate("/game/" + newId);
+  };
 
-    const submitHandler = (e) => {
-        e.preventDefault();
-
-        let newId;
-        createNewGame(name).then((id) => {
-            newId = id;
-            
-        })
-        new
-        navigate("/game/" + newId);
-    }
-
-
-	return ( 
+  return (
     <div>
-        <h1>Create Game</h1>
-        <input type="text" id="name" name="name" onChange={(e) => setName(e.target.value)}/>
-        <Button onClick={submitHandler}>Create Game</Button>
+      <h1 style={{ color: "white" }}>Create Game</h1>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Button onClick={submitHandler}>Create Game</Button>
     </div>
-    )
-   
-}
+  );
+};
 
 export default Create;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,25 @@
-import {Button} from 'react-bootstrap';
+import { Button } from "react-bootstrap";
 
 const Homepage = () => {
-    return (
-        <div className="homepage">
-	        <h1>Cards Against Computers</h1>
-            <Button href="/create">Create a Room</Button>
-            <Button href="/join">Join a Room</Button>
-        </div>
-    );
+  return (
+    <div className="homepage">
+      <h1 className="header">
+        Cards <br /> Against <br /> Computers.
+      </h1>
+
+      <h4 className="header" style={{ fontSize: "1em", marginLeft: "1.5em" }}>
+        A party game <br/> for horrible people.
+      </h4>
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "1em" }}
+      >
+        <Button href="/create">Create a Room</Button>
+        <Button href="/join" style={{ marginLeft: "1em" }}>
+          Join a Room
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
