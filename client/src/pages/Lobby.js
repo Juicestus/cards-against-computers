19bca1651cf4ece5865fb326dcb68577fbe56692
Juicestus <jus@gtsbr.org>
2023-05-04 09:29:15 -0500

    Lobby frontend

--- client/src/pages/Lobby.js
+++ client/src/pages/Lobby.js
@@ -4,9 +4,9 @@ import { loadLocalData } from "../util";
 import { queryBackend } from "../net";
 import { NavLink } from "react-router-dom";
 import { Card } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 const Lobby = () => {
-
   const navigate = useNavigate();
 
   const code = useParams().id;
@@ -19,21 +19,18 @@ const Lobby = () => {
 
   const [gameData, setGameData] = useState({});
 
-   
   useEffect(() => {
     queryBackend(
       "getGameData",
       {
         id: code,
         name: localData.userName,
-        privateKey: localData.privateKey
+        privateKey: localData.privateKey,
       },
       (content) => {
-        console.log(content);
         setGameData(content);
       }
     );
-
   }, [setGameData]);
 
   const userListElements = () => {
@@ -42,18 +39,50 @@ const Lobby = () => {
     if (players === undefined) {
       return "";
     }
-      console.log(host);
-    return Object.entries(players).map(([name, player]) => {
-      const classes = "lobby-card" + (name === host ? " lobby-host-color" : "");
+    console.log(host);
+    return (
+      <>
+        <h2 className="lobby-players-label">Players</h2>
+        {Object.entries(players).map(([name, player]) => {
+          const classes =
+            "lobby-card" + (name === host ? " lobby-host-color" : "");
+          return (
+            <div className={classes}>
+              <h2 className="lobby-card-text">
+                {name}
+                {name === host ? "  (host)" : ""}
+              </h2>
+            </div>
+          );
+        })}
+      </>
+    );
+  };
+
+  const startGameButton = () => {
+    if (gameData["host"] === localData.userName) {
       return (
-        <div className={classes}>
-          <h2 className="lobby-card-text">{name}{name === host ? "  (host)" : ""}</h2>
-        </div>
+        <Button className="lobby-start-game-button" onClick={startGameHandler}>
+          Start Game
+        </Button>
       );
-    });
-  }
+    }
+    return <div></div>;
+  };
 
-
+  const startGameHandler = () => {
+    queryBackend(
+      "startGame",
+      {
+        id: code,
+        name: localData.userName,
+        privateKey: localData.privateKey,
+      },
+      (content) => {
+        navigate("/game/" + code);
+      }
+    );
+  };
 
   return (
     <div className="page">
@@ -61,14 +90,12 @@ const Lobby = () => {
         <h2 className="create-join-back">
           <NavLink to="/">{"←"}</NavLink>
         </h2>
-        <h1 className="lobby-heading">
-          Lobby {code}.
-        </h1>
+        <h1 className="lobby-heading">Lobby {code}.</h1>
       </div>
       <div className="home-button-container">
+        {startGameButton()}
         {userListElements()}
       </div>
-      
     </div>
   );
 };
